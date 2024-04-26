5c9181c5989b111ab62bd35a154c64dcfddc979b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2024-04-26 12:25:17 +0100

    ✨Add global prefix and enable validation globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Set the global prefix for all routes
+  app.setGlobalPrefix('api/v1');
+
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
