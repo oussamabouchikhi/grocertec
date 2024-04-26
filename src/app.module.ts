089d19b766eb296c7a6a80bf8b227272847eff3b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2024-04-26 10:57:19 +0100

    ✨add Item CRUD boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { BreadModule } from './bread/bread.module';
 import { BeerModule } from './beer/beer.module';
 import { VegetableModule } from './vegetable/vegetable.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [BreadModule, BeerModule, VegetableModule],
+  imports: [BreadModule, BeerModule, VegetableModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
