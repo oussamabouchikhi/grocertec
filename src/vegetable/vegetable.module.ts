bdcde517aa4013999035bba22c06d0db822c15ac
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2024-04-28 21:10:30 +0100

    Remove Bread, Beer and Vegetable modules

--- src/vegetable/vegetable.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VegetableService } from './vegetable.service';
-import { VegetableController } from './vegetable.controller';
-
-@Module({
-  controllers: [VegetableController],
-  providers: [VegetableService],
-})
-export class VegetableModule {}
