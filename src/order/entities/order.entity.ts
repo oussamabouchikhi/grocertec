d9632091d365441278410118c86c042bb3ded1e3
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2024-05-04 14:23:50 +0100

    ♻️ Refcator Order entity

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,10 +1,15 @@
-import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Item } from '../../item/entities/item.entity';
+import { Receipt } from '../../receipt/entities/receipt.entity';
 
 @Entity()
 export class Order {
   @PrimaryGeneratedColumn()
   id: number;
+
   @OneToMany(() => Item, (item) => item.order)
   items: Item[];
+
+  @OneToOne(() => Receipt, (receipt) => receipt.order)
+  receipt: Receipt;
 }
